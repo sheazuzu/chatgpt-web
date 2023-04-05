85e65ab660fffcdf3afb1309a234e30eadbcc08c
sheazuzu <sheazuzu@hotmail.com>
2023-04-05 14:29:22 +0200

    individualisierung

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center text-neutral-500">
     <span>❤️ Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
+    <a href="https://github.com/sheazuzu/chatgpt-web" target="_blank" class="text-blue-500">
       GitHub
     </a>
   </div>
